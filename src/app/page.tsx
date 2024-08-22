09c7e3b370fa8f19b40404172a928cb05c047221
lloydchang <lloydchang@gmail.com>
2024-08-22 16:04:59 -0700

    feat(page.tsx): add
    Step 2: Create the main component
    Set up the main `Home` component:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 'use client'
 import { Box, Button, Stack, TextField } from '@mui/material'
 import { useState } from 'react'
+
+export default function Home() {
+  // We'll add more code here in the following steps
+}
