e15e7fa546f80efa295c8dd6f7a6bb3e64800cd8
lloydchang <lloydchang@gmail.com>
2024-08-22 15:35:34 -0700

    feat(route.js): add
    Step 2: Define the system prompt
    Add the system prompt that will guide our AI assistant’s behavior:

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -1,3 +1,9 @@
 import { NextResponse } from 'next/server'
 import { Pinecone } from '@pinecone-database/pinecone'
 import OpenAI from 'openai'
+
+const systemPrompt = `
+You are a rate my professor agent to help students find classes, that takes in user questions and answers them.
+For every user question, the top 3 professors that match the user question are returned.
+Use them to answer the question if needed.
+`
